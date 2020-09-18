20a334d10e22b9533357aa2bcca5df00fc63bc11
Ryan Ryder <ryanryder@gmail.com>
2020-09-18 11:59:25 -0400

    Initial commit

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit",
+         "apple":"round and crunchy"}
+
+#print(fruit)
+#print(fruit["lemon"])
+#fruit["pear"] = "an odd shaped apple"
+#print(fruit)
+#fruit["lime"] = "great with tequila"
+#print(fruit)
+#del fruit["lemon"]
+print(fruit)
+while True:
+    dict_key = input("Please enter a fruit:")
+    if dict_key == "quit":
+        break
+    description = fruit.get(dict_key)
+    print(description)
---
+++ motorbike.py
@@ -0,0 +1,3 @@
+bike = {"make": "Yamaha", "model": "YZ", "color": "blue", "engine_size": 250}
+print(bike["engine_size"])
+print(bike["color"])
