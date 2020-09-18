7694935c5f5dc44e4952bb8f89fe92fbdb9b052e
Ryan Ryder <ryanryder@gmail.com>
2020-09-18 14:54:29 -0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -2,26 +2,20 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "apple": "good for making cider",
          "lemon": "a sour, yellow citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
-         "lime": "a sour, green citrus fruit",
-         "apple":"round and crunchy"}
-#Playing with adding and edting dictionary values.
-#print(fruit)
-#print(fruit["lemon"])
-#fruit["pear"] = "an odd shaped apple"
-#print(fruit)
-#fruit["lime"] = "great with tequila"
-#print(fruit)
-#del fruit["lemon"]
+         "lime": "a sour, green citrus fruit"}
+
 print(fruit)
-while True:
-    dict_key = input("Please enter a fruit:")
-    if dict_key == "quit":
-        break
-    #default value if there is nothing in the dictionary will be send an error message.
-    description = fruit.get(dict_key, "We don't have a " + dict_key)
-    print(description)
-    #if dict_key in fruit:
-    #    description = fruit.get(dict_key)
-    #    print(description)
-    #else:
-    #    print("We don't have a " + dict_key)
+
+for i in range(10):
+    for snack in fruit:
+        print(snack + " is " + fruit[snack])
+    print('-' * 40)
+#while True:
+#    dict_key = input("Please enter a fruit:")
+#    if dict_key == "quit":
+#        break    
+#    if dict_key in fruit:
+#        description = fruit.get(dict_key)
+#        print(description)
+#    else:
+#        print("We don't have a " + dict_key)
